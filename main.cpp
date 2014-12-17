667a5ba0478475e0e0eb0b8ec6d53300c59c09fe
lucasb-eyer <lucasb.eyer.be@gmail.com>
2014-12-17 19:23:21 +0100

    Factor RealSense initialization out.

--- main.cpp
+++ main.cpp
@@ -14,8 +14,7 @@ PXCSenseManager *g_sm = nullptr;
 
 // Makes our error-checking life a little easier.
 bool pxc_verify(pxcStatus ret, std::string msg);
-// Current date and time as almost-ISO string.
-std::string now();
+bool init_realsense();
 
 int main(int argc, char **argv){
     // Initialize SDL
@@ -25,36 +24,9 @@ int main(int argc, char **argv){
     }
     std::atexit(SDL_Quit);
 
-    // Initialize RealSense
-    if ((g_sm = PXCSenseManager::CreateInstance()) == nullptr) {
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "RealSense Error", "Unable to create the SenseManager.", nullptr);
+    // Gets `g_sm` ready for recording what we need.
+    if (!init_realsense())
         return 2;
-    }
-    std::atexit([](){ g_sm->Release(); });
-
-    // Sets file recording or playback
-    char *pszPath = SDL_GetPrefPath("Beymans", "RealSenseRecorder");
-    if (!pszPath) {
-        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL Error", "Can't retrieve your home directory. What the!?", nullptr);
-        return 100;
-    }
-    std::string utf8path = pszPath + now() + ".rssdk";
-    SDL_free(pszPath);
-    std::cout << "Recording to " << utf8path << std::endl;
-
-    // Damn you RealSense!
-    std::wstring path = std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>>().from_bytes(utf8path);
-    if (!pxc_verify(g_sm->QueryCaptureManager()->SetFileName(path.c_str(), true), "Setting filename for recording."))
-        return 3;
-
-    // Chooses what streams we want to capture.
-    if (!pxc_verify(g_sm->EnableStream(PXCCapture::STREAM_TYPE_COLOR, 640, 480, 30), "Enabling RGB stream."))
-        return 4;
-    if (!pxc_verify(g_sm->EnableStream(PXCCapture::STREAM_TYPE_DEPTH, 640, 480, 30), "Enabling D stream. Yup Alex, can't get the D!"))
-        return 4;
-
-    if (!pxc_verify(g_sm->Init(), "Initialize the capture."))
-        return 5;
 
     // It seems that acquiring frames is necessary for the recording to record anything!
     // I tried just idling in a MessageBox and it didn't work.
@@ -64,7 +36,7 @@ int main(int argc, char **argv){
 
         // Waits until new frame is available and locks it for application processing.
         if (!pxc_verify(g_sm->AcquireFrame(true), "Acquiring frame"))
-            return 6;  // TODO: Apparently one should recover from PXC_STATUS_STREAM_CONFIG_CHANGED?
+            return 100;  // TODO: Apparently one should recover from PXC_STATUS_STREAM_CONFIG_CHANGED?
 
         // Done working with the frame.
         g_sm->ReleaseFrame();
@@ -84,6 +56,7 @@ bool pxc_verify(pxcStatus ret, std::string msg)
     return true;
 }
 
+// Current date and time as almost-ISO string.
 std::string now()
 {
     std::time_t rawtime;
@@ -92,4 +65,37 @@ std::string now()
     char buffer[80];
     std::strftime(buffer, 80, "%Y-%m-%d-%H-%M-%S", std::localtime(&rawtime));
     return std::string(buffer);
+}
+
+bool init_realsense()
+{
+    // Initialize RealSense
+    if ((g_sm = PXCSenseManager::CreateInstance()) == nullptr) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "RealSense Error", "Unable to create the SenseManager.", nullptr);
+        return false;
+    }
+    std::atexit([](){ g_sm->Release(); });
+
+    // Sets file recording or playback
+    char *pszPath = SDL_GetPrefPath("Beymans", "RealSenseRecorder");
+    if (!pszPath) {
+        SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "SDL Error", "Can't retrieve your home directory. What the!?", nullptr);
+        return false;
+    }
+    std::string utf8path = pszPath + now() + ".rssdk";
+    SDL_free(pszPath);
+    std::cout << "Recording to " << utf8path << std::endl;
+
+    // Damn you RealSense!
+    std::wstring path = std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>>().from_bytes(utf8path);
+    if (!pxc_verify(g_sm->QueryCaptureManager()->SetFileName(path.c_str(), true), "Setting filename for recording."))
+        return false;
+
+    // Chooses what streams we want to capture.
+    if (!pxc_verify(g_sm->EnableStream(PXCCapture::STREAM_TYPE_COLOR, 640, 480, 30), "Enabling RGB stream."))
+        return false;
+    if (!pxc_verify(g_sm->EnableStream(PXCCapture::STREAM_TYPE_DEPTH, 640, 480, 30), "Enabling D stream. Yup Alex, can't get the D!"))
+        return false;
+
+    return pxc_verify(g_sm->Init(), "Initialize the capture.");
 }
